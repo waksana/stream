b2f377d26472f385c5a12ff3a9d91513a1cb82d9
waksana <waksana@qq.com>
2019-07-10 02:53:34 +0000

    simplify api and make list truly lazy

--- README.md
+++ README.md
@@ -4,14 +4,11 @@ a js type for infinity lazy list and some method to control the list. 8 indicate
 ## EXAMPLE
 
 ```javascript
-var List = require('.');
-var assert = require('assert');
-
-var N = new List(1, a => a + 1); //nature number list
-var ones = new List(1, () => 1);
+var N = List.cons(1, () => N.map(n => n + 1)); //nature number list
+var ones = List.cons(1, () => ones);
 
 //combine header and a list into a list
-var Z = List.make(0, () => N); //Integer list
+var Z = List.cons(0, () => N); //Integer list
 
 assert.deepEqual(N.drop(5).take(5), [6, 7, 8, 9, 10]);
 assert.deepEqual(N.filter(a => a % 2 == 0).take(5), [2, 4, 6, 8, 10]);
@@ -20,12 +17,12 @@ assert.deepEqual(N.zipWith(Z, (n, z) => n * z).take(3), [0, 2, 6]);
 
 //get a infinite prime list (inefficient)
 var sieve = ls =>
-List.make(ls.head, () => sieve(ls.tail().filter(a => a % ls.head != 0)));
-var prime = sieve(N.tail());
+List.cons(ls.head, () => sieve(ls.tail.filter(a => a % ls.head != 0)));
+var prime = sieve(N.tail);
 assert.deepEqual(prime.take(5), [2, 3, 5, 7, 11]);
 
 //fibonacci
-var fibs = List.make(0, () => List.make(1, () => fibs.zipWith(fibs.tail(), (a, b) => a + b)));
+var fibs = List.cons(0, () => List.cons(1, () => fibs.zipWith(fibs.tail, (a, b) => a + b)));
 
 assert.deepEqual(fibs.take(6), [0, 1, 1, 2, 3, 5]);
 ```
--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-var List = module.exports = function(head, fn) {
-  this.head = head;
-  this.tail = () => {
-    if(!this._tail) this._tail = this.getTail();
-    return this._tail;
-  };
-  this.getTail = () => new List(fn(head), fn);
-}
+module.exports = class LazyList {
+  constructor(fn) {
+    this.visited = false;
+    this.fn = fn;
+  }
+  get value() {
+    if(!this.visited) {
+      this._value = this.fn();
+      this.visited = true;
+    }
+    return this._value;
+  }
+  get head() {
+    return this.value[0];
+  }
+  get tail() {
+    return this.value[1];
+  }
 
-List.make = function(head, tailfn) {
-  var list = new List(head);
-  list.getTail = tailfn;
-  return list;
+  static cons(head, tail) {
+    if(tail instanceof LazyList)
+      return new LazyList(() => [head, tail]);
+    else
+      return new LazyList(() => [head, tail()]);
+  }
+
+  drop(num) {
+    return new LazyList(() => {
+      return num === 0 ? this.value : this.tail.drop(num - 1).value;
+    });
+  }
+
+  map(fn) {
+    return new LazyList(() => [fn(this.head), this.tail.map(fn)]);
+  }
+
+  filter(fn) {
+    return new LazyList(() => {
+      if(fn(this.head))
+        return [this.head, this.tail.filter(fn)];
+      else
+        return this.tail.filter(fn).value;
+    }); 
+  }
+
+  zipWith(otherList, fn) {
+    return new LazyList(() => {
+      return [fn(this.head, otherList.head), this.tail.zipWith(otherList.tail, fn)];
+    });
+  }
+
+  take(num) {
+    if(num === 0) return [];
+    else return [this.head].concat(this.tail.take(num - 1));
+  }
 }
-List.prototype.drop = function(num) {
-  if(num == 0) return this;
-  else return this.tail().drop(num - 1);
-};
-List.prototype.map = function(fn) {
-  return List.make(fn(this.head), () => this.tail().map(fn));
-};
-List.prototype.filter = function(fn) {
-  if(fn(this.head)) return List.make(this.head, () => this.tail().filter(fn));
-  else return this.tail().filter(fn);
-};
-List.prototype.zipWith = function(list, fn) {
-  return List.make(fn(this.head, list.head), () => this.tail().zipWith(list.tail(), fn));
-};
-List.prototype.take = function(num) {
-  if(num == 0) return [];
-  else return [this.head].concat(this.tail().take(num - 1));
-};
